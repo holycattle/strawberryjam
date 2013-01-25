e6537e607d8daf824745fa978d6957ab2a847012
Joshua Castaneda <thejosh223@gmail.com>
2013-01-26 01:31:33 +0800

    More Stuff.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,9 +3,13 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	
-	public float rotationSpeed = 180;
-	public float moveSpeed = 1f;
-	public float moveSpeedMultiplier = 1f;
+	private float rotationSpeed = 180;
+	private float moveSpeed = 16f;
+	private float moveSpeedMultiplier = 1f;
+	
+	public bool controllable = false;
+	
+	private Vector3 velocity;
 	
 	private Transform trans;
 	private Rigidbody rigid;
@@ -15,11 +19,23 @@ public class Player : MonoBehaviour {
 		rigid = rigidbody;
 	}
 	
-	void Update () {
-		float turn = Input.GetAxis("Horizontal");
-		float move = Input.GetAxis("Vertical");
-		
-		trans.Rotate(new Vector3(0, turn * rotationSpeed * Time.deltaTime));
-		trans.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime);
+	void FixedUpdate () {
+		if (controllable) {
+			float turn = Input.GetAxis("Horizontal");
+			float move = Input.GetAxis("Vertical");
+			
+			trans.Rotate(new Vector3(0, turn * rotationSpeed * Time.fixedDeltaTime, 0));
+			
+			rigid.AddForce(transform.forward * move * moveSpeed * moveSpeedMultiplier);
+			
+//			velocity = Vector3.forward * move * moveSpeed * moveSpeedMultiplier * Time.fixedDeltaTime;
+//			trans.Translate(velocity);
+		}
+	}
+	
+	void OnCollisionEnter(Collision c) {
+//		rigid.AddForce(-rigid.velocity.normalized * 100);
+//		rigid.velocity = -rigid.velocity;
+		Debug.Log("Coll: " + gameObject.name);
 	}
 }
