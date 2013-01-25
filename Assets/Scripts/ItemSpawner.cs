2f9fadc98aa39c5c4232889ea8e7102d12b21c5e
Joshua Castaneda <thejosh223@gmail.com>
2013-01-26 02:59:55 +0800

    Food Drops added.

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class ItemSpawner : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+	const float MAP_SIZE = 10;
+	const float FOOD_INTERVAL = 4;
 	
-	}
+	public GameObject food;
+	
+	private float _timePassed;
 	
 	// Update is called once per frame
 	void Update () {
-	
+		_timePassed += Time.deltaTime;
+		if (_timePassed >= FOOD_INTERVAL) {
+			_timePassed -= FOOD_INTERVAL;
+			
+			Vector3 v = new Vector3(Random.Range(
+				-MAP_SIZE / 2, MAP_SIZE/2), 1, Random.Range(-MAP_SIZE / 2, MAP_SIZE/2));
+			
+			GameObject g = Instantiate(food, v, Quaternion.identity) as GameObject;
+		}
 	}
 }
