eb9ce640be95f1d9390e126ff884b63c216b7280
Joshua Castaneda <thejosh223@gmail.com>
2013-01-26 11:14:28 +0800

    Added kill tracking.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -32,8 +32,8 @@ public class Player : MonoBehaviour {
 	private AudioSource heartbeat;
 	private float stunRemaining;
 	
-//	public Player lastTouch;
-//	public float sinceTouch;
+	public Player lastTouch;
+	public float sinceTouch;
 	
 	private Transform trans;
 	private Rigidbody rigid;
@@ -60,6 +60,13 @@ public class Player : MonoBehaviour {
 			if (heartbeatTimer <= 0) {
 				audio.Play();
 				heartbeatTimer += heartbeatInterval;
+			}
+		}
+		
+		if (sinceTouch > 0) {
+			sinceTouch -= Time.deltaTime;
+			if (sinceTouch <= 0) {
+				lastTouch = null;	
 			}
 		}
 	}
@@ -145,6 +152,8 @@ public class Player : MonoBehaviour {
 			Debug.Log("Coll: " + gameObject.name);
 			Player p = c.gameObject.GetComponent<Player>();
 			c.rigidbody.velocity += rigid.velocity * knockbackMultiplier * p.knockbackResistor;
+			
+			lastTouch = p;
 		} else if (c.gameObject.tag == "Food") {
 			Debug.Log("Food!");
 			increaseFat(c.gameObject.GetComponent<Item>().fat);
