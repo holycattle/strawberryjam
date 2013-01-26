ca4a4faac395a2aacb16f2604dcf7c1024ef8357
Joshua Castaneda <thejosh223@gmail.com>
2013-01-26 13:43:12 +0800

    Added timer.

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameMode : MonoBehaviour {
+	
+	const float TIME_WIDTH = 0.2f;
+	const float TIME_HEIGHT = 0.05f;
+	const float ROUND_TIME = 10f;
+	
+	public Score[] scores;
+	
+	float timeRemaining;
+	bool gameEnded = false;
+	
+	void Start() {
+		timeRemaining = ROUND_TIME;
+		
+		GameObject[] g = GameObject.FindGameObjectsWithTag("Player");
+		scores = new Score[g.Length];
+		int i = 0;
+		foreach (GameObject gg in g) {
+			scores[i] = new Score(gg);
+			i++;
+		}
+	}
+	
+	void OnGUI() {
+		GUI.Box(new Rect(Screen.width - Screen.width * TIME_WIDTH, 0, Screen.width * TIME_WIDTH, Screen.height * TIME_HEIGHT),
+			"Time Remaining: " + Mathf.CeilToInt(timeRemaining));
+		
+		if (Input.GetKey(KeyCode.Tab) || gameEnded) {
+			int width = 100;
+			int height = 30;
+			for (int i = 0; i < scores.Length; i++) {
+				GUI.Box(new Rect((Screen.width - width) / 2, (Screen.height - height * scores.Length / 2f) / 2 + height * i,
+									width, height), scores[i].GetString());
+			}
+		}
+		
+		if (gameEnded) {
+			// Show Winner
+			
+		}
+	}
+	
+	void Update() {
+		timeRemaining -= Time.deltaTime;
+		if (timeRemaining <= 0) {
+			timeRemaining = ROUND_TIME;
+			
+			// End of Game
+			Time.timeScale = 0;
+			gameEnded = true;
+		}
+	}
+	
+	public Score[] GetWinner() {
+		int max = -1;
+		Score[] maxxes = new Score[scores.Length];
+		
+//		foreach(
+	}
+}
+
+public class Score {
+	public GameObject g;
+	public int kills;
+	public int deaths;
+	
+	public Score(GameObject gg) {
+		g = gg;	
+	}
+	
+	public string GetString() {
+		return g.name + " " + kills + "/" + deaths;
+	}
+}
--- Assets/Scripts/KillingFloor.cs
+++ Assets/Scripts/KillingFloor.cs
@@ -3,27 +3,7 @@ using System.Collections;
 
 public class KillingFloor : MonoBehaviour {
 	
-	public Score[] scores;
-	
 	void Start() {
-		GameObject[] g = GameObject.FindGameObjectsWithTag("Player");
-		scores = new Score[g.Length];
-		int i = 0;
-		foreach (GameObject gg in g) {
-			scores[i] = new Score(gg);
-			i++;
-		}
-	}
-	
-	void OnGUI() {
-		if (Input.GetKey(KeyCode.Tab)) {
-			int width = 100;
-			int height = 30;
-			for (int i = 0; i < scores.Length; i++) {
-				GUI.Box(new Rect((Screen.width - width) / 2, (Screen.height - height * scores.Length / 2f) / 2 + height * i,
-									width, height), scores[i].GetString());
-			}
-		}
 	}
 	
 	void OnCollisionEnter(Collision c) {
@@ -31,31 +11,13 @@ public class KillingFloor : MonoBehaviour {
 		Reviver r = c.gameObject.AddComponent<Reviver>();
 		r.remaining = 6f;
 		
-		Debug.Log("Someone Died: " + gameObject.name);
-		
 		// Add death + kill to counter
 		foreach (Score s in scores) {
-			Debug.Log(">>" + s.g.name);
 			if (s.g.name == c.gameObject.name) {
 				s.deaths++;
 			} else if (p.lastTouch != null && s.g.name == p.lastTouch.name) {
-				Debug.Log("Killered");
 				s.kills++;
 			}
 		}
-	}
-}
-
-public class Score {
-	public GameObject g;
-	public int kills;
-	public int deaths;
-	
-	public Score(GameObject gg) {
-		g = gg;	
-	}
-	
-	public string GetString() {
-		return g.name + " " + kills + "/" + deaths;
 	}
 }
