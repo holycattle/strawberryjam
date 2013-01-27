c5f66bb7358cb055e99e9d29283109d0d2e82051
Payton Yao <pepsiboy@gmail.com>
2013-01-27 12:43:42 +0800

    Forgot to put the timer on the respawn thing.

--- Assets/Scripts/KillingFloor.cs
+++ Assets/Scripts/KillingFloor.cs
@@ -3,22 +3,28 @@ using System.Collections;
 
 public class KillingFloor : MonoBehaviour {
 	
-	void Start() {
-	}
+	private float respawnTimer;
 	
+	void Start() {
+		respawnTimer = 0.50f;
+	}
 	void Update(){
-		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
-		foreach(GameObject go in players){
-			Vector3 position = go.rigidbody.position;
-			if(position.y < -20 && go.GetComponent<Reviver>() == null ){
-				Player p = go.GetComponent<Player>();
-				if (p.lastTouch != null) {
-					p.lastTouch.score.kills++;
+		respawnTimer -= Time.deltaTime;
+		if(respawnTimer <= 0){
+			respawnTimer = 0.50f;
+			GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+			foreach(GameObject go in players){
+				Vector3 position = go.rigidbody.position;
+				if(position.y < -20 && go.GetComponent<Reviver>() == null ){
+					Player p = go.GetComponent<Player>();
+					if (p.lastTouch != null) {
+						p.lastTouch.score.kills++;
+					}
+					p.score.deaths++;
+					p.transform.position = new Vector3(0, -1000, 0);
+					Reviver r = go.AddComponent<Reviver>();
+					r.remaining = 2f;
 				}
-				p.score.deaths++;
-				p.transform.position = new Vector3(0, -1000, 0);
-				Reviver r = go.AddComponent<Reviver>();
-				r.remaining = 2f;
 			}
 		}
 	}
