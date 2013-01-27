1d67f9f99857610f8a04f7186d6294dd07bb5528
Tim Joseph Dumol <tim@timdumol.com>
2013-01-27 10:31:47 +0800

    Add start manually.

--- Assets/Scripts/Wait.cs
+++ Assets/Scripts/Wait.cs
@@ -32,11 +32,15 @@ public class Wait : MonoBehaviour {
 	void OnGUI() {
 		var w = 1280; //Screen.width;
 		var h = 800; //Screen.height;
-		
+		var btnW  = 150;
+		var btnH = 50;
 		if (Networking.players[0] == Network.player) {
 			GUI.Label(new Rect(0, 0, w, 40),
 				"Please tell people to connect to: " + Network.player.ipAddress,
 				topLabelStyle);
+			if (GUI.Button (new Rect(w/2 - btnW/2, 100, btnW, btnH), "Start Game!")) {
+				networkView.RPC("StartGame", RPCMode.All);	
+			}
 		}
 		
 		GUI.Label(new Rect(0, 50, w, 40), 
