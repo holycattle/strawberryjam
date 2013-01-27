b743a4976bc2faffe328b7a33e797dd1c30fb5ca
Payton Yao <pepsiboy@gmail.com>
2013-01-27 12:41:37 +0800

    Fixed revive bug.

--- Assets/Scripts/KillingFloor.cs
+++ Assets/Scripts/KillingFloor.cs
@@ -6,6 +6,23 @@ public class KillingFloor : MonoBehaviour {
 	void Start() {
 	}
 	
+	void Update(){
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+		foreach(GameObject go in players){
+			Vector3 position = go.rigidbody.position;
+			if(position.y < -20 && go.GetComponent<Reviver>() == null ){
+				Player p = go.GetComponent<Player>();
+				if (p.lastTouch != null) {
+					p.lastTouch.score.kills++;
+				}
+				p.score.deaths++;
+				p.transform.position = new Vector3(0, -1000, 0);
+				Reviver r = go.AddComponent<Reviver>();
+				r.remaining = 2f;
+			}
+		}
+	}
+	
 	void OnCollisionEnter(Collision c) {
 		Player p = c.gameObject.GetComponent<Player>();
 		if (p.lastTouch != null) {
